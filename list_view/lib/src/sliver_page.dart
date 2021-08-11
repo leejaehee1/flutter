2f2b19575a9e904e9b83ec59ebb70b43e11d36a7
hanw001 <mosadi@nate.com>
2021-08-11 18:52:57 +0900

    edit tab_scroll_move

--- list_view/lib/src/sliver_page.dart
+++ list_view/lib/src/sliver_page.dart
@@ -22,11 +22,19 @@ class _SliverPageState extends State<SliverPage> with TickerProviderStateMixin {
   final page2Key = new GlobalKey();
   final page3Key = new GlobalKey();
 
+  double page1Height = 0;
+  double page2Height = 0;
+  double page3Height = 0;
+  bool isTapToScroll = false;
+
   @override
   void initState() {
     _tabController = TabController(length: 3, vsync: this);
     _scrollController = ScrollController();
-    _scrollController.addListener(() {});
+    _scrollController.addListener(() {
+      _onScroll();
+    });
+
     super.initState();
   }
 
@@ -39,7 +47,7 @@ class _SliverPageState extends State<SliverPage> with TickerProviderStateMixin {
           SliverAppBar(
             // expandedHeight: 100,
             floating: true,
-            pinned: true,
+            // pinned: true,
             backgroundColor: Colors.blue,
             title: CatalogAppBar(),
           ),
@@ -74,10 +82,38 @@ class _SliverPageState extends State<SliverPage> with TickerProviderStateMixin {
     );
   }
 
+  void _onScroll() {
+    if (isTapToScroll) return;
+
+    if (page1Key.currentContext != null) {
+      page1Height = page1Key.currentContext!.size!.height;
+    }
+    if (page2Key.currentContext != null) {
+      page2Height = page2Key.currentContext!.size!.height;
+    }
+    if (page3Key.currentContext != null) {
+      page3Height = page3Key.currentContext!.size!.height;
+    }
+
+    if (_scrollController.offset <= page1Height) {
+      _tabController.animateTo(0,
+          duration: const Duration(milliseconds: 0), curve: Curves.linear);
+    } else if (_scrollController.offset > page1Height &&
+        _scrollController.offset <= page1Height + page2Height) {
+      _tabController.animateTo(1,
+          duration: const Duration(milliseconds: 0), curve: Curves.linear);
+    } else if (_scrollController.offset > page1Height + page2Height &&
+        _scrollController.offset <= page1Height + page2Height + page3Height) {
+      _tabController.animateTo(2,
+          duration: const Duration(milliseconds: 0), curve: Curves.linear);
+    }
+  }
+
   void _onTapToScroll(int index) async {
     var keys = [page1Key, page2Key, page3Key];
     var previousIndex = _tabController.previousIndex;
 
+    isTapToScroll = true;
     if (index == 0) {
       await _scrollController.animateTo(
         0,
@@ -108,5 +144,6 @@ class _SliverPageState extends State<SliverPage> with TickerProviderStateMixin {
         }
       }
     }
+    isTapToScroll = false;
   }
 }
