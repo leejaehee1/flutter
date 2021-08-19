42112164bac2a6fc1686bebbf9d5f25634fbe812
leejaehee1 <54349213+leejaehee1@users.noreply.github.com>
2021-08-19 16:41:07 +0900

    Update login_pages.dart
    by john
    http 라이브러리 추가함
    login 코드 추가함

--- plms_start/lib/pages/login_pages.dart
+++ plms_start/lib/pages/login_pages.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 import 'home.dart';
 // import 'package:google_fonts/google_fonts.dart';
@@ -71,8 +73,24 @@ class _LoginPageState extends State<LoginPage> {
                           style: ElevatedButton.styleFrom(
                             primary: Color(0xff304D5B),
                           ),
-                          onPressed: () {
-                            Get.toNamed('/home');
+                          onPressed: () async {
+                            // 서버에서 username and password 비교를 해서 로그인 되어야 한다.
+                            // response.OK => Get.toNamed('/home'); and 세션(이 사용자와 관련한 정보를 가지고 있습니다.)
+                            var url =
+                            Uri.http('localhost:5000', '/api/login', {'userId': 'admin1', 'password' : 'admin1'});
+
+                            // Await the http get response, then decode the json-formatted response.
+                            var response = await http.get(url);
+                            if (response.statusCode == 200) {
+                              var jsonResponse =
+                              convert.jsonDecode(response.body) as Map<String, dynamic>;
+                              var itemCount = jsonResponse['totalItems'];
+                              print('Number of books about http: $itemCount.');
+                              Get.toNamed('/home');
+                            } else {
+                              print('Request failed with status: ${response.statusCode}.');
+                              // 에러 메시지를 사용자가 알 수 있게
+                            }
                           },
                           child: Text(
                             "Sign In",
