92ab71edd53256acaaec59ac4ca252f711c834e0
hanw001 <mosadi@nate.com>
2021-08-20 09:54:51 +0900

    edit complete_page_listview

--- plms_start/lib/pages/components/punch_complete.dart
+++ plms_start/lib/pages/components/punch_complete.dart
@@ -16,183 +16,211 @@ class _PunchCompleteState extends State<PunchComplete> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Header(),
-          Expanded(
-            child: Container(
+      appBar: AppBar(
+        backgroundColor: Color(0xff2B3745),
+        automaticallyImplyLeading: false,
+        title: Header(),
+      ),
+      body: DefaultTabController(
+        // initialIndex: 0,
+        length: 1,
+        child: Column(
+          children: [
+            Container(
               color: Color(0xFFE6E6E6),
               child: Container(
+                // height: MediaQuery.of(context).size.height,
+                height: 120,
                 width: MediaQuery.of(context).size.width,
-                // height: MediaQuery.of(context).size.height * 1 / 7,
                 // width: MediaQuery.of(context).size.width,
                 padding: EdgeInsets.all(10),
                 child: Container(
-                    color: Color(0xff2B3745),
-                    height: 30,
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  'Category',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  'aaaa',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              width: 60,
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  'Category',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  'aaaa',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  'Category',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  'aaaa',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              width: 60,
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  'Category',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  'aaaa',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  'Category',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  'aaaa',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              width: 60,
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  'Category',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  'aaaa',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Text("Comment of Not Accepted")
-                      ],
-                    )),
-              ),
-            ),
-          ),
-          Container(
-            color: Color(0xFFE6E6E6),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xffB7C5B9),
-                      offset: Offset(-7, 0),
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  color: Color(0xff2B3745),
+                  height: 30,
                   child: Column(
                     children: [
                       Row(
                         children: [
-                          TitleText(
-                            title: 'Attachment',
+                          Row(
+                            children: [
+                              Text(
+                                'Category',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                'aaaa',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 60,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Category',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                'aaaa',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'Category',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                'aaaa',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 60,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Category',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                'aaaa',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'Category',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                'aaaa',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 60,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Category',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                'aaaa',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                       SizedBox(
-                        height: 20,
+                        height: 30,
                       ),
-                      ImagePickers(),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      // _description(),
+                      Text("Comment of Not Accepted")
                     ],
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+            Expanded(
+              child: TabBarView(
+                children: [
+                  ListView(
+                    children: [
+                      Container(
+                        color: Color(0xFFE6E6E6),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0xffB7C5B9),
+                                  offset: Offset(-7, 0),
+                                ),
+                              ],
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      TitleText(
+                                        title: 'Attachment',
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  ImagePickers(),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  _description(),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Color(0xff2F4C5A), // background
+          // onPrimary: Colors.white, // foreground
+        ),
+        onPressed: () {
+          Get.offAllNamed('/home');
+        },
+        child: Text("Upload"),
       ),
     );
   }
