a1f9fd867dd51e4466736492454edd0248447278
hanw001 <mosadi@nate.com>
2021-08-20 17:58:51 +0900

    edit register textfield

--- plms_start/lib/pages/utils/textfield_register.dart
+++ plms_start/lib/pages/utils/textfield_register.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 
-class TextFieldRegister extends StatelessWidget {
-  String text;
+class TextFieldRegister extends StatefulWidget {
+  String title;
 
-  TextFieldRegister({Key? key, required this.text}) : super(key: key);
+  TextFieldRegister({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _TextFieldRegisterState createState() => _TextFieldRegisterState();
+}
+
+class _TextFieldRegisterState extends State<TextFieldRegister> {
+  final _txTextEditController = TextEditingController();
+
+  @override
+  void dispose() {
+    _txTextEditController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        SizedBox(width: 100, child: Text(text)),
+        SizedBox(width: 100, child: Text(widget.title)),
         SizedBox(
           width: 200,
           height: 30,
           child: TextField(
-              decoration: InputDecoration(
-            border: OutlineInputBorder(),
-          )),
+            controller: _txTextEditController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+            onChanged: (text) {
+              print(text);
+            },
+          ),
         ),
       ],
     );
