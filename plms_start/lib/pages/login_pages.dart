0fed970bbe66e51fcf799cb01f40bf7a108e8b9e
hanw001 <mosadi@nate.com>
2021-08-20 17:56:40 +0900

    edit login post code

--- plms_start/lib/pages/login_pages.dart
+++ plms_start/lib/pages/login_pages.dart
@@ -14,8 +14,15 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String name = '';
-  bool changeButton = false;
+  final _idTextEditController = TextEditingController();
+  final _pwTextEditController = TextEditingController();
+
+  @override
+  void dispose() {
+    _idTextEditController.dispose();
+    _pwTextEditController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,13 +63,21 @@ class _LoginPageState extends State<LoginPage> {
                   child: Column(
                     children: [
                       TextFormField(
+                        controller: _idTextEditController,
                         decoration: InputDecoration(
                             hintText: 'Enter UserName', labelText: 'UserName'),
+                        onChanged: (text) {
+                          setState(() {});
+                        },
                       ),
                       TextFormField(
+                        controller: _pwTextEditController,
                         obscureText: true,
                         decoration: InputDecoration(
                             hintText: 'Enter Password', labelText: 'Password'),
+                        onChanged: (text) {
+                          setState(() {});
+                        },
                       ),
                       SizedBox(
                         height: 30,
@@ -73,28 +88,49 @@ class _LoginPageState extends State<LoginPage> {
                           style: ElevatedButton.styleFrom(
                             primary: Color(0xff304D5B),
                           ),
-                          onPressed: () {
-                            Get.toNamed("/home");
-                          },
+                          // onPressed: () {
+                          //   Get.toNamed("/home");
+                          // },
                           // onPressed: () async {
                           //   // 서버에서 username and password 비교를 해서 로그인 되어야 한다.
                           //   // response.OK => Get.toNamed('/home'); and 세션(이 사용자와 관련한 정보를 가지고 있습니다.)
-                          //   var url =
-                          //   Uri.http('localhost:5000', '/api/login', {'userId': 'admin1', 'password' : 'admin1'});
+                          //   var url = Uri.http('10.0.2.2:5000', '/api/login',
+                          //       // {'userId': 'admin11', 'password': 'admin1'});
+                          //       {'userId': 'admin3', 'password': 'admin2'});
 
                           //   // Await the http get response, then decode the json-formatted response.
                           //   var response = await http.get(url);
                           //   if (response.statusCode == 200) {
                           //     var jsonResponse =
-                          //     convert.jsonDecode(response.body) as Map<String, dynamic>;
+                          //         convert.jsonDecode(response.body)
+                          //             as Map<String, dynamic>;
                           //     var itemCount = jsonResponse['totalItems'];
                           //     print('Number of books about http: $itemCount.');
+                          //     print('간다간다 쑝간다!');
                           //     Get.toNamed('/home');
                           //   } else {
-                          //     print('Request failed with status: ${response.statusCode}.');
+                          //     print(
+                          //         'Request failed with status: ${response.statusCode}.');
                           //     // 에러 메시지를 사용자가 알 수 있게
                           //   }
                           // },
+                          onPressed: () async {
+                            var url =
+                                Uri.parse('http://10.0.2.2:5000/api/login');
+                            var response = await http.post(url, body: {
+                              'userID': _idTextEditController.text,
+                              'password': _pwTextEditController.text,
+                            });
+                            print('Response status: ${response.statusCode}');
+                            print('Response body: ${response.body}');
+                            if (response.body == "true") {
+                              print('간다간다 쑝간다!');
+                              Get.toNamed('/home');
+                            } else {
+                              print("fail");
+                              _showDialog();
+                            }
+                          },
                           child: Text(
                             "Sign In",
                             style: TextStyle(
@@ -132,5 +168,33 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
         ));
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: new Text("Check your ID or Password"),
+          actions: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                new ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff71838D),
+                  ),
+                  child: new Text("Ok"),
+                  onPressed: () {
+                    Get.back();
+                  },
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
   }
 }
