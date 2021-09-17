48c0fac000f1fd82f69b735c54405f0de8162ec3
hanw001 <mosadi@nate.com>
2021-09-17 17:36:09 +0900

    getx data debug

--- plms_start/lib/screens/repository/data_repository.dart
+++ plms_start/lib/screens/repository/data_repository.dart
@@ -6,7 +6,8 @@ import 'package:plms_start/screens/models/model_system_list.dart';
 class DataRepository extends GetConnect {
   static DataRepository get to => Get.find();
 
-  var api = dotenv.env['PHONE_IP'];
+  // var api = dotenv.env['PHONE_IP'];
+  var api = dotenv.env['EMUL_IP'];
 
   @override
   void onInit() {
@@ -16,13 +17,36 @@ class DataRepository extends GetConnect {
 
   Future<SystemList> loadSystem() async {
     String url = '/summury/systems';
+    print('되냐?');
     final response = await get(url);
+    print('response: $response');
     print(response.body);
-    print(response.body[1]);
+    print('되냐????');
+    // print(response.body[1]);
+
     if (response.status.hasError) {
       return Future.error(response.status);
     } else {
+      print('되냐??????????????????');
       return SystemList.fromJson(response.body);
     }
   }
+  // Future<System> loadSystem() async {
+  //   List data = [];
+  //   // List body = response.body;
+  //   String url = '/summury/systems';
+  //   print('되냐?');
+  //   final response = await get(url);
+  //   List body = response.body;
+  //   print('response: ${response.runtimeType}');
+  //   print(body);
+  //   print('되냐????');
+  //   print(body[1]);
+  //   print('되냐??????????????????');
+
+  //   for (int i = 0; i < body.length; i++) {
+  //     data.add(System.fromJson(body[i]));
+  //   }
+  //   return data[1];
+  // }
 }
