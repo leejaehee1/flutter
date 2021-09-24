1bc9d1e0d5c528fac55a1b3dcfd742682391eb6f
HANW001 <mosadi@nate.com>
2021-09-24 14:21:15 +0900

    edit image painter

--- plms_start/lib/screens/utils/image_painter.dart
+++ plms_start/lib/screens/utils/image_painter.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-// import 'package:open_file/open_file.dart';
-// import 'package:path_provider/path_provider.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:image_painter/image_painter.dart';
 
@@ -19,31 +19,31 @@ class _ImagePaintersState extends State<ImagePainters> {
 
   String imageData = Get.arguments;
 
-  // void saveImage() async {
-  //   final image = await _imageKey.currentState.exportImage();
-  //   final directory = (await getApplicationDocumentsDirectory()).path;
-  //   await Directory('$directory/sample').create(recursive: true);
-  //   final fullPath =
-  //       '$directory/sample/${DateTime.now().millisecondsSinceEpoch}.png';
-  //   final imgFile = File('$fullPath');
-  //   imgFile.writeAsBytesSync(image);
-  //   ScaffoldMessenger.of(context).showSnackBar(
-  //     SnackBar(
-  //       backgroundColor: Colors.grey[700],
-  //       padding: const EdgeInsets.only(left: 10),
-  //       content: Row(
-  //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //         children: [
-  //           const Text("Image Exported successfully.",
-  //               style: TextStyle(color: Colors.white)),
-  //           TextButton(
-  //               onPressed: () => OpenFile.open("$fullPath"),
-  //               child: Text("Open", style: TextStyle(color: Colors.blue[200])))
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
+  void saveImage() async {
+    final image = await _imageKey.currentState!.exportImage();
+    final directory = (await getApplicationDocumentsDirectory()).path;
+    await Directory('$directory/sample').create(recursive: true);
+    final fullPath =
+        '$directory/sample/${DateTime.now().millisecondsSinceEpoch}.png';
+    final imgFile = File('$fullPath');
+    imgFile.writeAsBytesSync(image!);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.grey[700],
+        padding: const EdgeInsets.only(left: 10),
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Image Exported successfully.",
+                style: TextStyle(color: Colors.white)),
+            TextButton(
+                onPressed: () => OpenFile.open("$fullPath"),
+                child: Text("Open", style: TextStyle(color: Colors.blue[200])))
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,6 +60,10 @@ class _ImagePaintersState extends State<ImagePainters> {
             )
           ],
         ),
-        body: ImagePainter.file(File(imageData), key: _imageKey));
+        body: ImagePainter.file(
+          File(imageData),
+          key: _imageKey,
+          initialPaintMode: PaintMode.rect,
+        ));
   }
 }
