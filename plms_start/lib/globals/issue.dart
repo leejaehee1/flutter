177f5f61ff91fcfef8ae8027fccec0d12f13ea20
hanw001 <mosadi@nate.com>
2021-10-03 04:39:41 +0900

    add global data

---
+++ plms_start/lib/globals/issue.dart
@@ -0,0 +1,27 @@
+/*
+* name : globals
+* description : punch issue globals page.
+* writer : walter
+* create date : 2021-09-30
+* last update : 2021-09-30
+* */
+
+library plms.issue;
+
+// user data
+List<String> categoryList = [];
+List<String> systemsList = [];
+List<String> subsystemList = [];
+List<String> disciplineList = [];
+List<String> unitList = [];
+List<String> areaList = [];
+List<String> deptList = [];
+
+List<String> categoryNameList = [];
+List<String> systemsNameList = [];
+List<String> subsystemNameList = [];
+List<String> disciplineNameList = [];
+List<String> unitNameList = [];
+List<String> areaNameList = [];
+List<String> deptNameList = [];
+// List punch_issue_Material = [];
