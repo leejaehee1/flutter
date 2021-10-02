cbd372551997fda1b1e79038d480eb64ac7117c4
hanw001 <mosadi@nate.com>
2021-10-03 04:39:59 +0900

    edit confirm button

--- plms_start/lib/pages/utils/button_confirm.dart
+++ plms_start/lib/pages/utils/button_confirm.dart
@@ -8,46 +8,63 @@ class ConfirmButton extends StatefulWidget {
       {Key? key,
       // required this.name,
       required this.buttonname1,
-      required this.buttonname2})
+      required this.buttonname2,
+      required this.buttonname3})
       : super(key: key);
   // String name;
   final String buttonname1;
   final String buttonname2;
-
+  final String buttonname3;
   @override
   _ConfirmButtonState createState() => _ConfirmButtonState();
 }
 
 class _ConfirmButtonState extends State<ConfirmButton> {
+  var buttonWidth = Get.width * 1 / 3.5;
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xFFE6E6E6),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // crossAxisAlignment: CrossAxisAlignment.,
         children: [
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Color(0xff71838D),
+          Container(
+            width: buttonWidth,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff71838D),
+              ),
+              onPressed: () {
+                Get.back();
+              },
+              child: Text(widget.buttonname1),
             ),
-            onPressed: () {
-              _showDialog('/home');
-            },
-            child: Text(widget.buttonname1),
           ),
-          SizedBox(
-            width: 30,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Color(0xff2F4C5A), // background
-              // onPrimary: Colors.white, // foreground
+          Container(
+            width: buttonWidth,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff71838D),
+              ),
+              onPressed: () {
+                _showDialog('/home');
+              },
+              child: Text(widget.buttonname2),
             ),
-            onPressed: () {
-              Get.offAllNamed("/home");
-            },
-            child: Text(widget.buttonname2),
+          ),
+          Container(
+            width: buttonWidth,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff2F4C5A), // background
+                // onPrimary: Colors.white, // foreground
+              ),
+              onPressed: () {
+                Get.offAllNamed("/success");
+              },
+              child: Text(widget.buttonname3),
+            ),
           ),
         ],
       ),
